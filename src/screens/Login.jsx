6a72bde725482580321134ef79d3600421b15b0e
Daniel-MSoares <munizsoaresrapper@gmail.com>
2023-09-16 08:48:49 -0300

    teste ci

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,106 +1,110 @@
-import React, { useState } from "react";
-import { Button, Text, VStack, Icon, Input, useTheme } from "native-base";
-import { MaterialIcons } from "@expo/vector-icons";
-import { Image, Alert, TouchableOpacity, Linking } from "react-native";
-import {User} from '../services/user'
-import { useNavigation } from "@react-navigation/native";
+import React, { useState } from 'react'
+import { Button, Text, VStack, Icon, Input, useTheme } from 'native-base'
+import { MaterialIcons } from '@expo/vector-icons'
+import { Image, Alert, TouchableOpacity, Linking } from 'react-native'
+import { User } from '../services/user'
+import { useNavigation } from '@react-navigation/native'
 export function Login() {
-  const navigation = useNavigation();
-  const user = new User;
-  const { colors } = useTheme();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [inputType, setInputType] = useState("password");
-  const[isLoading,setIsLoading]=useState(false)
- 
+  const navigation = useNavigation()
+  const user = new User()
+  const { colors } = useTheme()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [inputType, setInputType] = useState('password')
+  const [isLoading, setIsLoading] = useState(false)
 
-  const submit = async() => {
+  const submit = async () => {
     setIsLoading(true)
-    if (email === "" || password === "") {
+    if (email === '' || password === '') {
       setIsLoading(false)
-      return Alert.alert("Erro", "por favor preencha todos os campos");
+      return Alert.alert('Erro', 'por favor preencha todos os campos')
     }
-    
-    await user.checkPassword(email,password)
-    .then(({data})=>{
-      if(!data.resultado){
+
+    await user
+      .checkPassword(email, password)
+      .then(({ data }) => {
+        if (!data.resultado) {
+          setIsLoading(false)
+          return Alert.alert('Erro', 'Usuário ou senha inválidos')
+        }
+        let jwtToken = data.token
         setIsLoading(false)
-        return Alert.alert("Erro", "Usuário ou senha inválidos");
-      }
-      let jwtToken=data.token
-      setIsLoading(false)
-      user.getUserByEmail(email,jwtToken)
-    })
-    .catch((error)=>{
-      setIsLoading(false)
-      if(!error.response.data.resultado){
-        return Alert.alert("Erro", "Usuário ou senha inválidos")
-      }
-      return Alert.alert("Erro", "Um erro inesperado aconteceu,tente novamente");
-    })
-    
-  };
+        user.getUserByEmail(email, jwtToken)
+      })
+      .catch((error) => {
+        setIsLoading(false)
+        if (!error.response.data.resultado) {
+          return Alert.alert('Erro', 'Usuário ou senha inválidos')
+        }
+        return Alert.alert(
+          'Erro',
+          'Um erro inesperado aconteceu,tente novamente'
+        )
+      })
+  }
 
   function handleVisibilityPassword() {
-    if (inputType == "password") {
-      setInputType("text");
+    if (inputType == 'password') {
+      setInputType('text')
     } else {
-      setInputType("password");
+      setInputType('password')
     }
   }
 
   return (
-    <VStack w="full" alignItems="center">
+    <VStack
+      w='full'
+      alignItems='center'
+    >
       <Image
-        source={require("../assets/logo.png")}
+        source={require('../assets/logo.png')}
         style={{ width: 187, height: 170 }}
-        resizeMode="contain"
+        resizeMode='contain'
       />
       <Text
-        alignSelf="flex-start"
+        alignSelf='flex-start'
         color={colors.blue[600]}
         ml={4}
-        fontFamily="heading"
+        fontFamily='heading'
       >
-        Fazer Login
+        Bem Vindo
       </Text>
       <Input
         onChangeText={setEmail}
         mt={4}
         height={54}
-        alignSelf="center"
+        alignSelf='center'
         w='94%'
         bgColor={colors.gray[100]}
         color={colors.blue[900]}
         leftElement={
           <Icon
             color={colors.blue[700]}
-            as={<MaterialIcons name="person" />}
+            as={<MaterialIcons name='person' />}
             size={6}
             ml={2}
           />
-          
         }
-        placeholder="Digite o seu email"
-        fontFamily={"Montserrat_500Medium"}
+        placeholder='Digite o seu email'
+        fontFamily={'Montserrat_500Medium'}
         placeholderTextColor={colors.blue[700]}
         fontSize={14}
         borderRadius={8}
-        autoCapitalize="none"
+        autoCapitalize='none'
       />
       <Input
         type={inputType}
         onChangeText={setPassword}
         mt={4}
         height={54}
-        alignSelf="center"
+        alignSelf='center'
         w='94%'
         bgColor={colors.gray[100]}
         color={colors.blue[900]}
         leftElement={
           <Icon
             color={colors.blue[700]}
-            as={<MaterialIcons name="lock" />}
+            as={<MaterialIcons name='lock' />}
             size={6}
             ml={2}
           />
@@ -111,7 +115,7 @@ export function Login() {
               color={colors.blue[700]}
               as={
                 <MaterialIcons
-                  name={inputType == "text" ? "visibility-off" : "visibility"}
+                  name={inputType == 'text' ? 'visibility-off' : 'visibility'}
                 />
               }
               size={6}
@@ -119,12 +123,12 @@ export function Login() {
             />
           </TouchableOpacity>
         }
-        placeholder="Senha"
-        fontFamily={"Montserrat_500Medium"}
+        placeholder='Senha'
+        fontFamily={'Montserrat_500Medium'}
         placeholderTextColor={colors.blue[700]}
         fontSize={14}
         borderRadius={8}
-        autoCapitalize="none"
+        autoCapitalize='none'
       />
       <Button
         bgColor={colors.blue[600]}
@@ -132,32 +136,32 @@ export function Login() {
         onPress={submit}
         height={54}
         mt={4}
-        w="90%"
+        w='90%'
         borderRadius={15}
         isLoading={isLoading}
       >
         Entrar
       </Button>
       <Button
-        onPress={() => navigation.navigate("Register")}
+        onPress={() => navigation.navigate('Register')}
         height={54}
         mt={4}
-        w="90%"
+        w='90%'
         borderRadius={15}
       >
         Cadastre-se
       </Button>
       <Button
         bgColor={colors.gray[200]}
-        _text={{ color: colors.blue[600],fontSize:'md' }}
+        _text={{ color: colors.blue[600], fontSize: 'md' }}
         height={54}
         mt={4}
-        w="90%"
+        w='90%'
         borderRadius={15}
-        onPress={() => navigation.navigate("PasswordRecovery")}
+        onPress={() => navigation.navigate('PasswordRecovery')}
       >
         Esqueci minha senha
       </Button>
     </VStack>
-  );
+  )
 }
