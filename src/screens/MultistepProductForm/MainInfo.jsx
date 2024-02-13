c5980f0c1cdc68eedfeea28688eeb83926c5008b
Daniel-MSoares <munizsoaresrapper@gmail.com>
2024-02-13 15:08:38 -0300

    add banner error loading form

--- src/screens/MultistepProductForm/MainInfo.jsx
+++ src/screens/MultistepProductForm/MainInfo.jsx
@@ -6,6 +6,7 @@ import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import { Keyboard, TouchableWithoutFeedback } from 'react-native';
+import { showMessage } from 'react-native-flash-message';
 import { ButtonBack } from '../../components/ButtonBack';
 import { ProgressBar } from './components/ProgressBar';
 import { LogoFeira } from '../../components/LogoFeira';
@@ -25,7 +26,7 @@ export function MainInfo() {
   const navigation = useNavigation();
   const productInstance = new Product();
   const user = useSelector((state) => state.AuthReducers.userData.userData);
-  
+
   const { colors } = useTheme();
 
   const {
@@ -42,7 +43,7 @@ export function MainInfo() {
 
   const handleCheckInfo = async (data) => {
     const price = await removeMoneyMask(data.preco);
-    data.produtor_id=user.id
+    data.produtor_id = user.id;
     data.preco = parseFloat(await price.replace(',', '.')).toFixed(2);
     if (parseFloat(data.preco) === 0) {
       return setError('preco', {
@@ -62,106 +63,113 @@ export function MainInfo() {
         setUnities(data[0].unidades);
         setFormLoaded(true);
       })
-      .catch((error) => console.log(error));
-
-    
+      .catch(() => {
+        showMessage({
+          message: 'um ero inesperado aconteceu',
+          type: 'danger',
+        });
+        navigation.goBack();
+      });
   };
 
   useFocusEffect(useCallback(initForm, []));
 
   return (
     <TouchableWithoutFeedback touchSoundDisabled onPress={() => Keyboard.dismiss()}>
-      {!FormLoaded ? <LoadingForm />:
-      <VStack w="full" h="full" px="3%">
-        <VStack>
-          <ButtonBack />
-          <LogoFeira />
-          <ProgressBar percent="25" />
-          <Text fontFamily="body" fontSize={RFValue(22)}>
-            Olá, {user.nome}!
-          </Text>
-          <Text
-            fontFamily="body"
-            fontSize={RFValue(22)}
-            textAlign="left"
-            textBreakStrategy="highQuality"
-          >
-            O que você está vendendo hoje?
-          </Text>
-        </VStack>
-
-        <VStack py={0}>
-          <InputLabel mt={errors.nome ? 0 : RFValue(4)} title="Nome do Produto" />
-          <ControlledInput
-            control={control}
-            error={errors.nome}
-            w="98%"
-            name="nome"
-            placeholder="Nome do Produto"
-            mt={1}
-          />
-
-          <InputLabel mt={errors.preco ? 0 : RFValue(4)} title="Preço do Produto" />
-          <ControlledInput
-            control={control}
-            error={errors.preco}
-            placeholder="R$ 0,00"
-            w="98%"
-            name="preco"
-            isMasked
-            type="money"
-            keyboardType="numeric"
-            options={{
-              precision: 2,
-              separator: ',',
-              delimiter: '.',
-              unit: 'R$ ',
-              suffixUnit: '',
-            }}
-            mt={1}
-          />
-          <InputLabel mt={errors.categoria ? 0 : RFValue(4)} title="Categoria do produto" />
-          <ControlledSelect
-            control={control}
-            w="98%"
-            name="categoria"
-            error={errors.categoria}
-            selectItemsValue={categories}
-            selectLabel="Selecione a categoria"
-            mt={1}
-          />
-          <InputLabel mt={errors.unidade ? 0 : RFValue(4)} title="Tipo da unidade" />
-          <ControlledSelect
-            control={control}
-            w="98%"
-            name="unidade"
-            error={errors.unidade}
-            selectItemsValue={unities}
-            selectLabel="Selecione a unidade"
-            mt={1}
-          />
-        </VStack>
-
-        <VStack>
-          <Button
-            alignSelf="center"
-            w="98%"
-            mt={8}
-            _pressed={{ bgColor: colors.blue[700] }}
-            borderRadius={8}
-            onPress={handleSubmit(handleCheckInfo)}
-          >
-            <Text
-              color={colors.gray[100]}
-              fontWeight="semibold"
-              fontFamily="body"
-              fontSize={RFValue(18)}
-            >
-              Continuar
+      {!FormLoaded ? (
+        <LoadingForm />
+      ) : (
+        <VStack w="full" h="full" px="3%">
+          <VStack>
+            <ButtonBack />
+            <LogoFeira />
+            <ProgressBar percent="25" />
+            <Text fontFamily="body" fontSize={RFValue(22)}>
+              Olá, {user.nome}!
             </Text>
-          </Button>
+            <Text
+              fontFamily="body"
+              fontSize={RFValue(22)}
+              textAlign="left"
+              textBreakStrategy="highQuality"
+            >
+              O que você está vendendo hoje?
+            </Text>
+          </VStack>
+
+          <VStack py={0}>
+            <InputLabel mt={errors.nome ? 0 : RFValue(4)} title="Nome do Produto" />
+            <ControlledInput
+              control={control}
+              error={errors.nome}
+              w="98%"
+              name="nome"
+              placeholder="Nome do Produto"
+              mt={1}
+            />
+
+            <InputLabel mt={errors.preco ? 0 : RFValue(4)} title="Preço do Produto" />
+            <ControlledInput
+              control={control}
+              error={errors.preco}
+              placeholder="R$ 0,00"
+              w="98%"
+              name="preco"
+              isMasked
+              type="money"
+              keyboardType="numeric"
+              options={{
+                precision: 2,
+                separator: ',',
+                delimiter: '.',
+                unit: 'R$ ',
+                suffixUnit: '',
+              }}
+              mt={1}
+            />
+            <InputLabel mt={errors.categoria ? 0 : RFValue(4)} title="Categoria do produto" />
+            <ControlledSelect
+              control={control}
+              w="98%"
+              name="categoria"
+              error={errors.categoria}
+              selectItemsValue={categories}
+              selectLabel="Selecione a categoria"
+              mt={1}
+            />
+            <InputLabel mt={errors.unidade ? 0 : RFValue(4)} title="Tipo da unidade" />
+            <ControlledSelect
+              control={control}
+              w="98%"
+              name="unidade"
+              error={errors.unidade}
+              selectItemsValue={unities}
+              selectLabel="Selecione a unidade"
+              mt={1}
+            />
+          </VStack>
+
+          <VStack>
+            <Button
+              alignSelf="center"
+              w="98%"
+              mt={8}
+              _pressed={{ bgColor: colors.blue[700] }}
+              borderRadius={8}
+              onPress={handleSubmit(handleCheckInfo)}
+            >
+              <Text
+                color={colors.gray[100]}
+                fontWeight="semibold"
+                fontFamily="body"
+                fontSize={RFValue(18)}
+              >
+                Continuar
+              </Text>
+            </Button>
+          </VStack>
         </VStack>
-      </VStack>}
+      )}
     </TouchableWithoutFeedback>
   );
 }
