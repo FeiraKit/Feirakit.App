c2399c8dc1c3283d97d996de67f19dc00040c36c
Daniel Muniz <munizsoaresrapper@gmail.com>
2025-06-06 14:56:04 -0300

    remove log

--- src/routes/login.route.jsx
+++ src/routes/login.route.jsx
@@ -22,7 +22,7 @@ export function LoginRoutes() {
   useEffect(() => {
     VerifyIfUserAlreadyUsedTheApp();
   }, []);
-  console.log(AlreadyUsed);
+
   return (
     <>
       {isUndefined(AlreadyUsed) && <Loading />}
